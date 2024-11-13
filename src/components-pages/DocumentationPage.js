42d89ed2052294e92ce4fc21d330f199ad12cb1f
unknown <skemalt@outlook.com>
2024-11-13 20:42:24 +0300

    your comment

--- src/components-pages/DocumentationPage.js
+++ src/components-pages/DocumentationPage.js
@@ -1,5 +1,6 @@
 import Docs from "../components/Docs.js";
 import projectProposal from "../assets/Project Proposal.pdf";
+import projectSpecificationsReport from "../assets/Project Specifications Report.pdf";
 import "../styles/doc-cards.css";
 
 const DocumentationsPage = () => (
@@ -8,6 +9,7 @@ const DocumentationsPage = () => (
      
 
       <Docs name={"Project Proposal"} link={projectProposal} />
+      <Docs name={"Project Specifications Report"} link={projectSpecificationsReport} />
     </div>
 
     {/* <iframe className='' src={projectProposal} height="100%" width="100%" title="Project Proposal"></iframe> */}
