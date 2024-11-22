7c022454e5007e8acd9311d0fa173cb388629cfe
unknown <skemalt@outlook.com>
2024-11-22 22:24:46 +0300

    new document added

--- src/components-pages/DocumentationPage.js
+++ src/components-pages/DocumentationPage.js
@@ -1,6 +1,7 @@
 import Docs from "../components/Docs.js";
 import projectProposal from "../assets/Project Proposal.pdf";
 import projectSpecificationsReport from "../assets/Project Specifications Report.pdf";
+import ProjectAnalysisReport from "../assets/Project Analysis Report.pdf";
 import "../styles/doc-cards.css";
 
 const DocumentationsPage = () => (
@@ -10,6 +11,7 @@ const DocumentationsPage = () => (
 
       <Docs name={"Project Proposal"} link={projectProposal} />
       <Docs name={"Project Specifications Report"} link={projectSpecificationsReport} />
+      <Docs name={"ProjectAnalysisReport"} link={ProjectAnalysisReport} />
     </div>
 
     {/* <iframe className='' src={projectProposal} height="100%" width="100%" title="Project Proposal"></iframe> */}
