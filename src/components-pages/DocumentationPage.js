20efbe54fbb7023bc17796296753e699ce5e218a
unknown <skemalt@outlook.com>
2024-12-31 00:36:13 +0300

    committed

--- src/components-pages/DocumentationPage.js
+++ src/components-pages/DocumentationPage.js
@@ -2,6 +2,7 @@ import Docs from "../components/Docs.js";
 import projectProposal from "../assets/Project Proposal.pdf";
 import projectSpecificationsReport from "../assets/Project Specifications Report.pdf";
 import ProjectAnalysisReport from "../assets/Project Analysis Report.pdf";
+import HighLevelDesignReport from "../assets/High Level Design Report.pdf";
 import "../styles/doc-cards.css";
 
 const DocumentationsPage = () => (
@@ -11,7 +12,8 @@ const DocumentationsPage = () => (
 
       <Docs name={"Project Proposal"} link={projectProposal} />
       <Docs name={"Project Specifications Report"} link={projectSpecificationsReport} />
-      <Docs name={"ProjectAnalysisReport"} link={ProjectAnalysisReport} />
+      <Docs name={"Project Analysis Report"} link={ProjectAnalysisReport} />
+      <Docs name={"High Level Design Report"} link={HighLevelDesignReport} />
     </div>
 
     {/* <iframe className='' src={projectProposal} height="100%" width="100%" title="Project Proposal"></iframe> */}
